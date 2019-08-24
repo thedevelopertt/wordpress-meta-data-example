0d366d59f83c547beae7626994fb50bad0547663
Greg <greigdillon@gmail.com>
2019-08-24 08:59:03 -0500

    Added MetaTags to header file Added description in index file

--- header.php
+++ header.php
@@ -1,7 +1,57 @@
+<?php
+
+function _showTags()
+{
+//        Shows all tags if available
+
+    if (get_the_tags()) {
+        $tags = array();
+        foreach (get_the_tags() as $tag) {
+            $tags . array_push($tags, $tag->name);
+        }
+        echo join(',', $tags);
+    }
+}
+
+function showTags()
+{
+    _showTags();
+}
+
+?>
+
+
 <html>
 <head>
 
+    <link rel="canonical" href="<?php echo the_permalink() ?>"/>
 
+    <!--    Basic MetaTags-->
+    <title><?php echo get_the_title(); ?></title>
+    <meta name="description" content="<?php echo get_the_excerpt(); ?>">
+    <meta name="keywords" content="<?php showTags(); ?>">
+    <!--    Basic MetaTags-->
+
+
+    <!--        Begin Facebook OpenGraph-->
+    <meta property="og:url" content="<?php echo get_permalink(); ?>">
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="<?php echo get_the_title(); ?>">
+    <meta property="og:description" content="<?php echo get_the_excerpt(); ?>">
+    <meta property="og:image"
+          content="<?php echo wp_get_upload_dir()['baseurl']; ?>/<?php echo get_the_ID(); ?>_facebook.jpg">
+    <meta property="og:image:type" content="jpg">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <!--        End Facebook OpenGraph-->
+
+    <!--        Start Twitter Card-->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@thedevelopertt">
+    <meta name="twitter:creator" content="@thedevelopertt">
+    <meta name="twitter:image"
+          content="<?php echo wp_get_upload_dir()['baseurl']; ?>/<?php echo get_the_ID(); ?>_twitter.jpg">
+    <!--        End Twitter Card-->
 
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php get_header(); ?>
+<h1>
+    Wordpress MetaData Example
+</h1>
+<p>
+    This example shows how <a href="thedevelopertt.ml">The Developer</a> adds meta tags to the articles.
+</p>
 <?php get_footer(); ?>
